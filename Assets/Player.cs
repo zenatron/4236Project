9100f896f943bb97de165145d189fee551a63514
Phil Vishnevsky <gadgetsphilop36@gmail.com>
2024-10-26 17:04:39 -0400

    Update player animations and fix jumping and movement bugs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour 
@@ -7,8 +8,15 @@ public class Player : MonoBehaviour
     private Animator anim;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
-
     private float xInput;
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    private bool isGrounded;
+
     void Start() 
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,8 +27,15 @@ public class Player : MonoBehaviour
     {
         Movement();
         CheckInput();
+        CollisionChecks();
 
+        FlipController();
         AnimatorControllers();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundMask);
     }
 
     private void CheckInput()
@@ -35,18 +50,42 @@ public class Player : MonoBehaviour
 
     private void Movement() 
     {
-        rb.velocity = new Vector2(xInput * 2, rb.velocity.y);
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
 
     private void Jump() 
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     private void AnimatorControllers() 
     {
         bool isMoving = rb.velocity.x != 0;
 
+        anim.SetFloat("yVelocity", rb.velocity.y);
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private void Flip()
+    {
+        facingDir *= -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        } 
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
     }
 }
